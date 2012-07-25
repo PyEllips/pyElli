20c590d8ac66ea72343d238acdb5547b26608673
O. Castany <olivier.castany@telecom-bretagne.eu>
2012-07-25 15:40:29 +0200

    Added comments

--- examples/cholesteric.py
+++ examples/cholesteric.py
@@ -22,26 +22,31 @@ n_med = (ne + no)/2
 LC = Berreman4x4.UniaxialNonDispersiveMaterial(no, ne)
 R = Berreman4x4.rotation_v_theta([0,1,0], pi/2)
 LC = LC.rotated(R)
-p = 0.65e-6      # cholesteric pitch
+# Cholesteric pitch:
+p = 0.65e-6
+# One half turn of a right-handed helix:
 TN = Berreman4x4.TwistedMaterial(LC, p/2, angle=+pi, div=40)
 
 # Inhomogeneous layer, repeated layer, and structure
 IL = Berreman4x4.InhomogeneousLayer(TN)
-n = 25      # number of repetitions (half pitches)
-L = Berreman4x4.RepeatedLayers([IL], n)
+N = 25      # number half pitch repetitions
+h = N * p/2
+L = Berreman4x4.RepeatedLayers([IL], N)
 s = Berreman4x4.Structure(front, [L], back)
 
 # Normal incidence: 
 Kx = 0.0
 
 # Calculation parameters
-lbda_min, lbda_max = 0.6e-6, 1.5e-6   # (m)
+lbda_min, lbda_max = 0.8e-6, 1.2e-6   # (m)
+lbda_B = p * n_med
 lbda_list = numpy.linspace(lbda_min, lbda_max, 100)
 k0_list = 2*pi/lbda_list
 
 ############################################################################
-# Analytical calculation
-R_th = (numpy.tanh(Dn*n*pi**2/(k0_list)))**2
+# Analytical calculation for the maximal reflection
+R_th = numpy.tanh(Dn/n_med*pi*h/p)**2
+lbda_B1, lbda_B2 = p*no, p*ne
 
 ############################################################################
 # Calculation with Berreman4x4
@@ -57,21 +62,23 @@ R_sp = Berreman4x4.extractCoefficient(power, 'r_sp')
 Jc = Berreman4x4.circularJones(J)
 power = abs(Jc)**2
 
+# Right-circular wave is reflected in the stop-band
 # R_LR, T_LR close to zero
 R_RR = Berreman4x4.extractCoefficient(power, 'r_RR')
 T_RR = Berreman4x4.extractCoefficient(power, 't_RR')
 
-# T_RL, R_RL, R_LL close to zero
-# T_LL close to 1
+# Left-circular wave is transmitted in the full spectrum
+# T_RL, R_RL, R_LL close to zero, T_LL close to 1
 T_LL = Berreman4x4.extractCoefficient(power, 't_LL')
 R_LL = Berreman4x4.extractCoefficient(power, 'r_LL')
-
 
 ############################################################################
 # Plotting
 fig = pyplot.figure()
-# pyplot.rcParams['axes.color_cycle'] = ['b','b','b','b','g','r']
 ax = fig.add_subplot("111")
+
+rectangle = pyplot.Rectangle((lbda_B1,0), lbda_B2-lbda_B1, R_th, color='cyan')
+ax.add_patch(rectangle)
 
 # d1 = numpy.vstack((T_pp, R_pp, T_sp, R_sp)).T
 # d2 = numpy.vstack((T_RR, R_RR, T_LR, R_LR)).T
@@ -84,10 +91,9 @@ ax = fig.add_subplot("111")
 
 ax.plot(lbda_list, R_RR, label='R_RR')
 ax.plot(lbda_list, T_RR, label='T_RR')
+ax.legend(loc='center right', bbox_to_anchor=(1.00, 0.50))
 
-# ax.legend(lines2, legend2, 
-#           loc='upper left', bbox_to_anchor=(1.05, 1), borderaxespad=0.)
-ax.set_title(u"Cholesteric Liquid Crystal, {:d} helix full turns".format(n))
+ax.set_title(u"Cholesteric Liquid Crystal, {:.1f} helix pitches".format(N/2.))
 ax.set_xlabel(r"Wavelength $\lambda_0$ (m)")
 ax.set_ylabel(r"Power transmission $T$ and reflexion $R$")
 fmt = ax.xaxis.get_major_formatter()
