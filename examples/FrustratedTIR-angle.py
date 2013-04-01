ddf222135b9bb2f2a9a61cc0abf89c9f76774dd0
O. Castany <olivier.castany@telecom-bretagne.eu>
2013-04-01 11:38:03 +0200

    Backup during rewriting FrustratedTIR documentation

--- examples/FrustratedTIR-angle.py
+++ examples/FrustratedTIR-angle.py
@@ -12,6 +12,7 @@ from Berreman4x4 import c, pi
 from numpy import exp, cos, arcsin, real, sqrt
 import matplotlib.pyplot as pyplot
 
+
 print("\n*** Glass1 / Air / Glass2 ***\n")
 
 ############################################################################
@@ -19,12 +20,12 @@ print("\n*** Glass1 / Air / Glass2 ***\n")
 
 # Refractive indices
 n_f = 1.5
-n   = 1.0
+n_s = 1.0
 n_b = 1.7
 
 # Materials:
 glass1 = Berreman4x4.IsotropicNonDispersiveMaterial(n_f)
-air    = Berreman4x4.IsotropicNonDispersiveMaterial(n)
+air    = Berreman4x4.IsotropicNonDispersiveMaterial(n_s)
 glass2 = Berreman4x4.IsotropicNonDispersiveMaterial(n_b)
 
 # Layer and half-spaces:
@@ -38,51 +39,52 @@ s = Berreman4x4.Structure(front, [layer], back)
 # Wavelength and wavenumber:
 lbda = 1e-6
 k0 = 2*pi/lbda
-Phi_i = pi/2 * 0.6   # Incidence angle (higher than the limit angle)
 
-# Air thickness variation range
-h = numpy.linspace(0, 1.0e-6)
+# Layer thickness
+d = lbda*0.347
+layer.setThickness(d)
+
+# Variation of incidence angle
+Phi_list = numpy.linspace(0, pi/2*0.999)
+Kx = front.get_Kx_from_Phi(Phi_list)
 
 ############################################################################
 # Analytical calculation
-
-Kx = n_f*numpy.sin(Phi_i) # Reduced wavenumber
-
 # Incidence angle
-Phi_b = arcsin((Kx)/n_b)
-Phi_n = arcsin((complex(Kx/n)))
+Phi_s = arcsin((Kx/n_s).astype(complex))
+Phi_b = arcsin((Kx/n_b).astype(complex))
 
 # Wave vector:
-kz_f = n_f*k0*cos(Phi_i)
-kz_n = k0*sqrt(complex(n**2 - Kx**2))
+kz_f = n_f*k0*cos((Phi_list.astype(complex)))
+kz_s = k0*sqrt((-(Kx**2 - n_s**2)).astype(complex))
 kz_b = n_b*k0*cos(Phi_b)
 
-# Amplitude coefficient polarisation s:
-r_nfs = (kz_f-kz_n)/(kz_n+kz_f)
-r_bns = (kz_n-kz_b)/(kz_n+kz_b)
+# Amplitude coefficient for 's' polarisation:
+r_nfs = (kz_f-kz_s)/(kz_s+kz_f)
+r_bns = (kz_s-kz_b)/(kz_s+kz_b)
 t_nfs = 1+r_nfs
 t_bns = 1+r_bns
 
-# Amplitude coefficient polarisation p:
-r_nfp = (kz_f*n**2-kz_n*n_f**2)/(kz_n*n_f**2+kz_f*n**2)
-r_bnp = (kz_n*n_b**2-kz_b*n**2)/(kz_n*n_b**2+kz_b*n**2)
-t_nfp = cos(Phi_i)*(1-r_nfp)/cos(Phi_n)
-t_bnp = cos(Phi_n)*(1-r_bnp)/cos(Phi_b)
+# Amplitude coefficient for 'p' polarisation:
+r_nfp = (kz_f*n_s**2-kz_s*n_f**2)/(kz_s*n_f**2+kz_f*n_s**2)
+r_bnp = (kz_s*n_b**2-kz_b*n_s**2)/(kz_s*n_b**2+kz_b*n_s**2)
+t_nfp = cos((Phi_list.astype(complex)))*(1-r_nfp)/cos(Phi_s)
+t_bnp = cos(Phi_s)*(1-r_bnp)/cos(Phi_b)
 
 # Power coefficients:
-R_th_ss = (abs((r_nfs+r_bns*exp(2j*kz_n*h)) \
-                /(1+r_bns*r_nfs*exp(2j*kz_n*h))))**2
+R_th_ss = (abs((r_nfs+r_bns*exp(2j*kz_s*d)) \
+          /(1+r_bns*r_nfs*exp(2j*kz_s*d))))**2
 
-t2_th_ss = (abs((t_bns*t_nfs*exp(1j*kz_n*h)) \
-                /(1+r_bns*r_nfs*exp(2j*kz_n*h))))**2
+t2_th_ss = (abs((t_bns*t_nfs*exp(1j*kz_s*d)) \
+          /(1+r_bns*r_nfs*exp(2j*kz_s*d))))**2
 
-R_th_pp = (abs((r_nfp+r_bnp*exp(2j*kz_n*h)) \
-                /(1+r_bnp*r_nfp*exp(2j*kz_n*h))))**2
+R_th_pp = (abs((r_nfp+r_bnp*exp(2j*kz_s*d)) \
+          /(1+r_bnp*r_nfp*exp(2j*kz_s*d))))**2
 
-t2_th_pp= (abs((t_bnp*t_nfp*exp(1j*kz_n*h)) \
-                /(1+r_bnp*r_nfp*exp(2j*kz_n*h))))**2
+t2_th_pp= (abs((t_bnp*t_nfp*exp(1j*kz_s*d)) \
+          /(1+r_bnp*r_nfp*exp(2j*kz_s*d))))**2
 
-correction = real(n_b*cos(Phi_b)/(n_f*cos(Phi_i)))
+correction = real(n_b*cos(Phi_b)/(n_f*cos(Phi_list.astype(complex))))
 # This is a correction term used in R +T*correction = 1
 
 T_th_ss = t2_th_ss*correction
@@ -90,16 +92,8 @@ T_th_pp = t2_th_pp*correction
 
 ############################################################################
 # Calculation with Berreman4x4
+data = numpy.array([s.getJones(kx,k0) for kx in Kx])
 
-Kx = front.get_Kx_from_Phi(Phi_i, k0)   # Reduced wavenumber
-
-l = []
-for h_i in h:
-    layer.setThickness(h_i)
-    l.append(s.getJones(Kx,k0))
-data = numpy.array(l)
-
-# Extraction of the transmission and reflexion coefficients
 data = abs(data)**2
 R_pp  = Berreman4x4.extractCoefficient(data, 'r_pp')
 R_ss  = Berreman4x4.extractCoefficient(data, 'r_ss')
@@ -114,23 +108,21 @@ fig = pyplot.figure(figsize=(12., 6.))
 pyplot.rcParams['axes.color_cycle'] = ['b','g','r','c','b','g']
 ax = fig.add_axes([0.1, 0.1, 0.7, 0.8])
 
-d = numpy.vstack((R_ss,R_pp,t2_ss,t2_pp,T_ss,T_pp)).T
+y = numpy.vstack((R_ss,R_pp,t2_ss,t2_pp,T_ss,T_pp)).T
 legend1 = ("R_ss","R_pp","t2_ss","t2_pp","T_ss","T_pp")
-lines1 = ax.plot(h,d)
+lines1 = ax.plot(Kx, y)
 
-d = numpy.vstack((R_th_ss,R_th_pp,t2_th_ss,t2_th_pp,T_th_ss,T_th_pp)).T
-legend2 = ("R_th_ss","R_th_pp","t2_th_ss","t2_th_pp","T_th_ss","T_th_pp")
-lines2 = ax.plot(h,d, 'x')
+y_th = numpy.vstack((R_th_ss, R_th_pp, t2_th_ss, t2_th_pp,
+                  T_th_ss, T_th_pp)).T
+legend2 = ("R_th_ss", "R_th_pp", "t2_th_ss", "t2_th_pp",
+           "T_th_ss", "T_th_pp")
+lines2 = ax.plot(Kx, y_th, 'x')
 
 ax.legend(lines1 + lines2, legend1 + legend2, 
           loc='upper left', bbox_to_anchor=(1.05, 1), borderaxespad=0.)
 
-ax.set_title("FTIR: Glass1 / Air (h) / Glass2, for incidence angle " +
-             "$\Phi_i$ = {:.0f}$^\circ$".format(Phi_i*180/pi))
-ax.set_xlabel(r"Air layer thickness, $h$ (m)")
+ax.set_title("FTIR: Glass1 / Air ($d$ = {:.3g} m) / Glass2".format(d))
+ax.set_xlabel(r"Reduced wave number, $Kx$")
 ax.set_ylabel(r"Reflexion and transmission coefficients $R$, $T$")
-fmt = ax.xaxis.get_major_formatter()
-fmt.set_powerlimits((-3,3))
 pyplot.show()
-
 
