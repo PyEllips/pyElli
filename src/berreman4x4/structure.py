16a588720f65ab228c6e0cd2794c78d9c8c3fb7a
Marius Müller <49639740+MarJMue@users.noreply.github.com>
2021-04-20 18:07:53 +0200

    Rework Structure class

--- src/berreman4x4/structure.py
+++ src/berreman4x4/structure.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 from .evaluation import Evaluation
-from .half_spaces import IsotropicHalfSpace
+from .experiment import Experiment
 
 
 class Structure:
@@ -14,7 +14,7 @@ class Structure:
     """
     frontHalfSpace = None
     backHalfSpace = None
-    layers = None  # list of layers
+    layers = []  # list of layers
 
     def __init__(self, front=None, layers=None, back=None):
         """Creates an empty structure.
@@ -23,25 +23,21 @@ class Structure:
         'layers' : layer list, see setLayers()
         'back' : back half space, see setBackHalfSpace()
         """
-        self.layers = []  # list of layers
-        if front is not None:
-            self.setFrontHalfSpace(front)
-        if layers is not None:
-            self.setLayers(layers)
-        if back is not None:
-            self.setBackHalfSpace(back)
+        self.setFrontHalfSpace(front)
+        self.setLayers(layers)
+        self.setBackHalfSpace(back)
 
     def setFrontHalfSpace(self, halfSpace):
-        """Defines the front half-space.
+        """Defines the front half-space material.
 
-        'halfSpace' : HalfSpace object
+        'material' : Material object
         """
         self.frontHalfSpace = halfSpace
 
     def setBackHalfSpace(self, halfSpace):
-        """Defines the back half-space.
+        """Defines the back half-space material.
 
-        'halfSpace' : HalfSpace object
+        'material' : Material object
         """
         self.backHalfSpace = halfSpace
 
@@ -52,93 +48,6 @@ class Structure:
         """
         self.layers = layers
 
-    def getPropagationMatrix(self, Kx, k0, inv=False):
-        """Gives the propagation matrix of the structure.
-
-        'Kx' : reduced wavenumber along x
-        'k0' : wavenumber in vacuum
-        'inv' : returns propagation matrix for decreasing z
-
-        Returns : propagation matrix P(zb,zf) for the full structure
-
-        Psi(zb) = P_(zb, z_{N-1}) * ... * P(z1,zf) * Psi(zf)
-                = P(zb,zf) * Psi(zf)
-        """
-        if inv:
-            layers = reversed(self.layers)
-        else:
-            layers = self.layers
-        P_tot = np.identity(4)
-        # Cumulative products :
-        for L in layers:
-            P = L.getPropagationMatrix(Kx, k0, inv)
-            P_tot = P @ P_tot
-        return P_tot
-
-    def getStructureMatrix(self, Kx, k0):
-        """Returns the transfer matrix T of the structure.
-
-        [Eis, Ers, Eip, Erp].T = T * [c1, c2, c3, c4].T
-        T = Lf^-1 * P(zf,zb) * Lb
-        """
-        ILf = self.frontHalfSpace.getTransitionMatrix(Kx, k0, inv=True)
-        P = self.getPropagationMatrix(Kx, k0, inv=True)
-        Lb = self.backHalfSpace.getTransitionMatrix(Kx, k0)
-        T = ILf @ P @ Lb
-        return T
-
-    def getJones(self, Kx, k0):
-        """Returns the Jones matrices.
-
-        Returns : tuple (T_ri, T_ti)
-
-        T_ri is the Jones matrix for reflexion : [[r_pp, r_ps],
-                                                  [r_sp, r_ss]]
-
-        T_ti is the Jones matrix for transmission : [[t_pp, t_ps],
-                                                     [t_sp, t_ss]]
-
-        Naming convention (Fujiwara, p. 220):
-        't_ps' : transmitted 'p' component for an 's' incident wave
-        't_ss' : transmitted 's' component for an 's' incident wave
-        ...
-
-        Note: If all materials are isotropic, r_ps = r_sp = t_sp = t_ps = 0
-
-        See also:
-        * extractCoefficient() to extract the desired coefficients.
-        * circularJones() for circular polarization basis
-        """
-        T = self.getStructureMatrix(Kx, k0)
-        # Extraction of T_it out of T. "2::-2" means integers {2,0}.
-        T_it = T[:, 2::-2, 2::-2]
-        # Calculate the inverse and make sure it is a matrix.
-        T_ti = np.linalg.inv(T_it)
-
-        # Extraction of T_rt out of T. "3::-2" means integers {3,1}.
-        T_rt = T[:, 3::-2, 2::-2]
-
-        # Then we have T_ri = T_rt * T_ti
-        T_ri = T_rt @ T_ti
-        return T_ri, T_ti
-
-    def getPowerTransmissionCorrection(self, Kx, k0):
-        """Returns correction coefficient for power transmission
-
-        The power transmission coefficient is the ratio of the 'z' components
-        of the Poynting vector:       T = P_t_z / P_i_z
-        For isotropic media, we have: T = kb'/kf' |t_bf|^2
-        The correction coefficient is kb'/kf'
-
-        Note : For the moment it is only meaningful for isotropic half spaces.
-        """
-        Kzf = self.frontHalfSpace.get_Kz_from_Kx(Kx, k0)
-        if isinstance(self.backHalfSpace, IsotropicHalfSpace):
-            Kzb = self.backHalfSpace.get_Kz_from_Kx(Kx, k0)
-            return Kzb.real / Kzf.real
-        else:
-            return None
-
     def evaluate(self, lbda, theta_i, unit='nm'):
         """Return the Evaluation of the structure for the given parameters"""
-        return Evaluation(self, lbda, theta_i, unit)
+        return Evaluation(Experiment(self, None, theta_i, lbda, unit))
