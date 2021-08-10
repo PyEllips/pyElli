873365752a89afd7e1730e684549d994ccd9551d
dobener <florian.dobener@physik.uni-giessen.de>
2021-08-10 08:20:39 +0200

    Added docstrings in utils for testing

--- src/berreman4x4/utils.py
+++ src/berreman4x4/utils.py
@@ -13,6 +13,20 @@ from .dispersions import DispersionTableEpsilon
 
 
 def calcPseudoDiel(rho, angle, output='eps'):
+    """Calculates the pseudo dielectric function of a measurement from rho.
+
+    Args:
+        rho (pandas.DataFrame): Measurement DataFrame containing rho as complex number as column and wavelength as index
+        angle (float): Angle of measurement in degree
+        output (str, optional): Output format for dielectric function. 
+            'n': refractive index, 
+            'eps': Dielectic function as two-column pandas.DataFrame,
+            'epsi': Dielectric function as imaginary number.
+            Defaults to 'eps'.
+
+    Returns:
+        pandas.DataFrame: Frame containing the pseudo dielectric function or refractive index.
+    """
     theta = angle * np.pi / 180
     eps = np.sin(theta)**2 * (1 + np.tan(theta)**2 * ((1 - rho) / (1 + rho))**2)
 
@@ -29,6 +43,19 @@ def calcPseudoDiel(rho, angle, output='eps'):
 
 
 def calc_rho(psi_delta):
+    """Calculate rho from a Psi-Delta DataFrame. The Psi-Delta DataFrame should be structured as follows:
+        index: Wavelength
+        column 'Ψ': Psi from measurement
+        column 'Δ': Delta from measurement
+
+        This format is as returned from SpectraRay.read_psi_delta_file(...).
+
+    Args:
+        psi_delta (pandas.DataFrame): DataFrame containing Psi+Delta Measurement data
+
+    Returns:
+        pandas.DataFrame: Frame containing rho as an imaginary number.
+    """
     return psi_delta.apply(lambda x: np.tan(np.deg2rad(x['Ψ'])) *
                                np.exp(-1j * np.deg2rad(x['Δ'])),
                                axis=1)
