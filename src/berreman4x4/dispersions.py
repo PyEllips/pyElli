Corrects sign conventions

src/berreman4x4/dispersions.py
@@ -30,16 +30,6 @@ class DispersionLaw(ABC):
         """Add up the dielectric function of multiple models"""
         return DispersionSum(self, other)
 
-    def getDielectricConj(self, lbda: npt.ArrayLike) -> npt.NDArray:
-        """Returns the conjugated dielectric constant for wavelength 'lbda' default unit (nm)
-        in the convention ε1 - iε2."""
-        return np.conjugate(self.dielectricFunction(lbda))
-
-    def getRefractiveIndexConj(self, lbda: npt.ArrayLike) -> npt.NDArray:
-        """Returns the conjugated refractive index for wavelength 'lbda' default unit (nm)
-        in the convention n - ik."""
-        return sqrt(np.conjugate(self.dielectricFunction(lbda)))
-
     def getDielectric(self, lbda: npt.ArrayLike) -> npt.NDArray:
         """Returns the dielectric constant for wavelength 'lbda' default unit (nm)
         in the convention ε1 + iε2."""
@@ -253,10 +243,11 @@ class DispersionGauss(DispersionLaw):
     def dielectricFunction(self, lbda: npt.ArrayLike) -> npt.NDArray:
         E = lambda2E(lbda)
         ftos = 2 * sqrt(np.log(2))
-        return np.conjugate(self.eps_inf + sum(2 * Ai / sqrt(np.pi) * (dawsn(ftos * (E + Ei) / gami) - dawsn(ftos * (E - Ei) / gami)) -
-                                               1j * (Ai * np.exp(-(ftos * (E - Ei) / gami)**2) -
-                                                     Ai * np.exp(-(ftos * (E + Ei) / gami)**2))
-                                               for Ai, Ei, gami in self.coeffs))
+        return self.eps_inf + sum(2 * Ai / sqrt(np.pi) *
+                            (dawsn(ftos * (E + Ei) / gami) - dawsn(ftos * (E - Ei) / gami)) +
+                            1j * (Ai * np.exp(-(ftos * (E - Ei) / gami)**2) -
+                                    Ai * np.exp(-(ftos * (E + Ei) / gami)**2))
+                            for Ai, Ei, gami in self.coeffs)
 
 
 class DispersionTaucLorentz(DispersionLaw):
@@ -298,9 +289,9 @@ class DispersionTaucLorentz(DispersionLaw):
         E = lambda2E(lbda)
         Eg = self.coeffs[0]
         eps_inf = self.coeffs[1]
-        return eps_inf + np.conjugate(sum(1j * (Ai * Ei * Ci * (E - Eg)**2 / ((E**2 - Ei**2)**2 + Ci**2 * E**2) / E) * np.heaviside(E - Eg, 0) +
-                                          self.eps2(E, Eg, Ai, Ei, Ci)
-                                          for Ai, Ei, Ci in self.coeffs[2:]))
+        return eps_inf + sum(1j * (Ai * Ei * Ci * (E - Eg)**2 / ((E**2 - Ei**2)**2 + Ci**2 * E**2) / E) * np.heaviside(E - Eg, 0) +
+                             self.eps2(E, Eg, Ai, Ei, Ci)
+                             for Ai, Ei, Ci in self.coeffs[2:])
 
 
 class DispersionHighEnergyBands(DispersionLaw):
@@ -322,7 +313,7 @@ class DispersionHighEnergyBands(DispersionLaw):
         eps_i = 3 * self.E_xsi * (np.abs(E) - self.E_xsi)**2 / E**5 * \
             np.heaviside(np.abs(E) - self.E_xsi, 0)
 
-        return self.A * (eps_r - 1j * eps_i)
+        return self.A * (eps_r + 1j * eps_i)
 
 
 class DispersionTanguy(DispersionLaw):
@@ -350,11 +341,11 @@ class DispersionTanguy(DispersionLaw):
 
     def dielectricFunction(self, lbda: npt.ArrayLike) -> npt.NDArray:
         E = lambda2E(lbda)
-        return np.conjugate(1 + self.a / (self.b - E**2) +
-                            self.A * self.R**(self.d/2 - 1) / (E + 1j * self.gam)**2 *
-                            (DispersionTanguy.g(DispersionTanguy.xsi(E + 1j * self.gam, self.R, self.Eg), self.d) +
-                             DispersionTanguy.g(DispersionTanguy.xsi(-E - 1j * self.gam, self.R, self.Eg), self.d) -
-                             2 * DispersionTanguy.g(DispersionTanguy.xsi(E*0, self.R, self.Eg), self.d)))
+        return (1 + self.a / (self.b - E**2) +
+                self.A * self.R**(self.d/2 - 1) / (E + 1j * self.gam)**2 *
+                (DispersionTanguy.g(DispersionTanguy.xsi(E + 1j * self.gam, self.R, self.Eg), self.d) +
+                    DispersionTanguy.g(DispersionTanguy.xsi(-E - 1j * self.gam, self.R, self.Eg), self.d) -
+                    2 * DispersionTanguy.g(DispersionTanguy.xsi(E*0, self.R, self.Eg), self.d)))
 
     @staticmethod
     def xsi(z, R, Eg):

src/berreman4x4/solver4x4.py
@@ -228,44 +228,44 @@ class Solver4x4(Solver):
             L += np.tile(np.array([[0, 0, 0, 0],
                                    [0, 0, 0, 0],
                                    [1, 0, 0, 0],
-                                   [1, 0, 0, 0]]), (i, 1, 1)) / cos_Phi[:, None, None]
+                                   [-1, 0, 0, 0]]), (i, 1, 1)) / cos_Phi[:, None, None]
             L += np.tile(np.array([[0, 0, 0, 0],
                                    [0, 0, 0, 0],
                                    [0, 0, 0, 1],
-                                   [0, 0, 0, -1]]), (i, 1, 1)) / nx[:, None, None]
+                                   [0, 0, 0, 1]]), (i, 1, 1)) / nx[:, None, None]
             L += np.tile(np.array([[0, 0, -1, 0],
                                    [0, 0, 1, 0],
                                    [0, 0, 0, 0],
                                    [0, 0, 0, 0]]), (i, 1, 1)) / cos_Phi[:, None, None] / nx[:, None, None]
             return 0.5 * L
-            #   np.array(
+            #   0.5 * np.array(
             # [[0, 1, -1/(nx*cos_Phi),  0],
             #  [0, 1,  1/(nx*cos_Phi),  0],
             #  [1/cos_Phi, 0,  0,  1/nx],
-            #  [1/cos_Phi, 0,  0, -1/nx]])
-        else:
-            L = np.tile(np.array([[0, 0, 0, 0],
-                                  [1, 1, 0, 0],
-                                  [0, 0, 0, 0],
-                                  [0, 0, 0, 0]], dtype=np.complex128), (i, 1, 1))
-            L += np.tile(np.array([[0, 0, 1, 1],
-                                   [0, 0, 0, 0],
-                                   [0, 0, 0, 0],
-                                   [0, 0, 0, 0]]), (i, 1, 1)) * cos_Phi[:, None, None]
-            L += np.tile(np.array([[0, 0, 0, 0],
-                                   [0, 0, 0, 0],
-                                   [0, 0, 0, 0],
-                                   [0, 0, 1, -1]]), (i, 1, 1)) * nx[:, None, None]
-            L += np.tile(np.array([[0, 0, 0, 0],
-                                   [0, 0, 0, 0],
-                                   [-1, 1, 0, 0],
-                                   [0, 0, 0, 0]]), (i, 1, 1)) * cos_Phi[:, None, None] * nx[:, None, None]
-            return L
+            #  [-1/cos_Phi, 0,  0, 1/nx]])
+
+        L = np.tile(np.array([[0, 0, 0, 0],
+                                [1, 1, 0, 0],
+                                [0, 0, 0, 0],
+                                [0, 0, 0, 0]], dtype=np.complex128), (i, 1, 1))
+        L += np.tile(np.array([[0, 0, 1, -1],
+                                [0, 0, 0, 0],
+                                [0, 0, 0, 0],
+                                [0, 0, 0, 0]]), (i, 1, 1)) * cos_Phi[:, None, None]
+        L += np.tile(np.array([[0, 0, 0, 0],
+                                [0, 0, 0, 0],
+                                [0, 0, 0, 0],
+                                [0, 0, 1, 1]]), (i, 1, 1)) * nx[:, None, None]
+        L += np.tile(np.array([[0, 0, 0, 0],
+                                [0, 0, 0, 0],
+                                [-1, 1, 0, 0],
+                                [0, 0, 0, 0]]), (i, 1, 1)) * cos_Phi[:, None, None] * nx[:, None, None]
+        return L
             # np.array(
-            # [[0, 0, cos_Phi, cos_Phi],
+            # [[0, 0, cos_Phi, -cos_Phi],
             #  [1, 1, 0, 0],
             #  [-nx*cos_Phi, nx*cos_Phi, 0, 0],
-            #  [0, 0, nx, -nx]])
+            #  [0, 0, nx, nx]])
 
     @staticmethod
     def getKz(material: "Material", lbda: npt.ArrayLike, Kx: npt.ArrayLike) -> npt.NDArray:
@@ -286,7 +286,7 @@ class Solver4x4(Solver):
 
     @property
     def delta(self) -> npt.NDArray:
-        d = -np.angle(self.rho, deg=True)
+        d = np.angle(self.rho, deg=True)
         return np.where(d < 0, d + 360, d)
 
     @property
@@ -299,7 +299,7 @@ class Solver4x4(Solver):
 
     @property
     def deltaMat(self) -> npt.NDArray:
-        return -np.angle(self.rhoMat, deg=True)
+        return np.angle(self.rhoMat, deg=True)
 
     @property
     def mueller_matrix(self) -> npt.NDArray:
@@ -368,21 +368,19 @@ class Solver4x4(Solver):
         """Simulates optical Experiment"""
         layers = reversed(self.permProfile[1:-1])
 
-        ILf = self.TransitionMatrixIsoHalfspace(self.Kx, self.permProfile[0], inv=True)
+        if isinstance(self.structure.backMaterial, IsotropicMaterial):
+            T = self.TransitionMatrixIsoHalfspace(self.Kx, self.permProfile[-1])
+        else:
+            T = self.TransitionMatrixHalfspace(
+                self.buildDeltaMatrix(self.Kx, self.permProfile[-1]))
 
-        P_tot = np.identity(4)
         for d, epsilon in layers:
             P = self.propagator.calculate_propagation(
                 self.buildDeltaMatrix(self.Kx, epsilon), -d, self.lbda)
-            P_tot = P @ P_tot
+            T = P @ T
 
-        if isinstance(self.structure.backMaterial, IsotropicMaterial):
-            Lb = self.TransitionMatrixIsoHalfspace(self.Kx, self.permProfile[-1])
-        else:
-            Lb = self.TransitionMatrixHalfspace(
-                self.buildDeltaMatrix(self.Kx, self.permProfile[-1]))
-
-        T = ILf @ P_tot @ Lb
+        Lf = self.TransitionMatrixIsoHalfspace(self.Kx, self.permProfile[0], inv=True)
+        T = Lf @ T
 
         # Extraction of T_it out of T. "2::-2" means integers {2,0}.
         T_it = T[:, 2::-2, 2::-2]
@@ -397,7 +395,6 @@ class Solver4x4(Solver):
 
         r_ss = T_ri[..., 1, 1]
         S = T_ri / r_ss[:, None, None]
-        S[..., 0, :] = -S[..., 0, :]
 
         Kzf = self.getKz(self.structure.frontMaterial, self.lbda, self.Kx)
         if isinstance(self.structure.backMaterial, IsotropicMaterial):