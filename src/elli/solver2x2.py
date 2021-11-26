28aebc885abc9636d023b28c69b2dbfb50fd0ea0
MarJMue <49639740+MarJMue@users.noreply.github.com>
2021-11-26 23:36:07 +0100

    Include power correction for Solver2x2

--- src/elli/solver2x2.py
+++ src/elli/solver2x2.py
@@ -77,7 +77,7 @@ class Solver2x2(Solver):
                                                [zeros, ttots]]), 2, 0)
 
         # TODO: Calculate power_correction factor
-        power_correction =  np.ones_like(self.lbda)
+        power_correction = (((n_list[-1] * np.cos(th_list[-1])).real) / (n_list[0] * np.cos(th_list[0])).real)
 
         return Result(self.experiment, jones_matrix_r, jones_matrix_t, power_correction)
 
