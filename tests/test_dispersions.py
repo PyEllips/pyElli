f29feb82e0b79f365f5177c9d10b91307231484b
dobener <florian.dobener@physik.uni-giessen.de>
2022-02-21 17:38:02 +0100

    Dispersions test blueprint

---
+++ tests/test_dispersions.py
@@ -0,0 +1,57 @@
+"""Tests for dispersion models"""
+
+
+def test_constant_refr_index():
+    """Tests constant refractive index"""
+
+
+def test_epsilon_inf():
+    """Tests epsilon infinity / constant dielectric function"""
+
+
+def test_cauchy():
+    """Tests cauchy dispersion"""
+
+
+def test_sellmeier():
+    """Tests sellmeier dispersion"""
+
+
+def test_drude_energy():
+    """Tests Drude energy dispersion"""
+
+
+def test_drude_resistivity():
+    """Tests Drude resistivity dispersion"""
+
+
+def test_lorentz_lambda():
+    """Test Lorentz dispersion with wavelength parameters"""
+
+
+def test_lorentz_energy():
+    """Test Lorentz dispersion with energy parameters"""
+
+
+def test_gauss():
+    """Test Gauss dispersion"""
+
+
+def test_tauc_lorentz():
+    """Test Tauc-Lorentz dispersion"""
+
+
+def test_tanguy():
+    """Test Tanguy dispersion"""
+
+
+def test_poles():
+    """Test Poles dispersion"""
+
+
+def test_table_refr_index():
+    """Test refractive index dispersion table"""
+
+
+def test_table_epsilon():
+    """Test epsilon dispersion table"""
