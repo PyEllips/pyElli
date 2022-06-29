14075abf7cd6744fae41d24e87d54f0d77ade4e7
domna <florian.dobener@physik.hu-berlin.de>
2022-06-29 21:32:41 +0200

    Dispersions docstring cleanup

--- src/elli/materials_db.py
+++ src/elli/materials_db.py
@@ -1,5 +1,5 @@
 # Encoding: utf-8
 from .materials import IsotropicMaterial
-from .dispersions.dispersions import ConstantRefractiveIndex, EpsilonInf
+from .dispersions.dispersions import ConstantRefractiveIndex
 
 AIR = IsotropicMaterial(ConstantRefractiveIndex(n=1))
