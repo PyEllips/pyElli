e69e5ac0cf09454ddf9a0c8e9722fd76bb9c966a
domna <florian.dobener@physik.hu-berlin.de>
2022-07-25 14:15:15 +0200

    Fixes indentation in calc_rho

--- src/elli/utils.py
+++ src/elli/utils.py
@@ -4,7 +4,8 @@ import numpy as np
 import scipy.constants as sc
 from numpy.lib.scimath import sqrt
 
-def calc_pseudo_diel(rho, angle: float, output: str = 'eps') -> pd.DataFrame:
+
+def calc_pseudo_diel(rho, angle: float, output: str = "eps") -> pd.DataFrame:
     """Calculates the pseudo dielectric function of a measurement from rho.
 
     Args:
@@ -21,28 +22,28 @@ def calc_pseudo_diel(rho, angle: float, output: str = 'eps') -> pd.DataFrame:
         pandas.DataFrame: Frame containing the pseudo dielectric function or refractive index.
     """
     theta = angle * np.pi / 180
-    eps = np.sin(theta)**2 * (1 + np.tan(theta)**2 * ((1 - rho) / (1 + rho))**2)
+    eps = np.sin(theta) ** 2 * (1 + np.tan(theta) ** 2 * ((1 - rho) / (1 + rho)) ** 2)
 
-    if output == 'n':
+    if output == "n":
         n = sqrt(eps)
-        return pd.DataFrame({'n': n.real,
-                             'k': n.imag}, index=eps.index)
+        return pd.DataFrame({"n": n.real, "k": n.imag}, index=eps.index)
 
-    if output == 'epsi':
+    if output == "epsi":
         return eps
 
-    return pd.concat({'ϵ1': eps.apply(lambda x: x.real),
-                      'ϵ2': eps.apply(lambda x: x.imag)}, axis=1)
+    return pd.concat(
+        {"ϵ1": eps.apply(lambda x: x.real), "ϵ2": eps.apply(lambda x: x.imag)}, axis=1
+    )
 
 
 def calc_rho(psi_delta: pd.DataFrame) -> pd.DataFrame:
     """Calculate rho from a Psi-Delta DataFrame.
-            The Psi-Delta DataFrame should be structured as follows:
-        index: Wavelength
-        column 'Ψ': Psi from measurement
-        column 'Δ': Delta from measurement
+    The Psi-Delta DataFrame should be structured as follows:
+        :index: Wavelength
+        :column 'Ψ': Psi from measurement
+        :column 'Δ': Delta from measurement
 
-        This format is as returned from SpectraRay.read_psi_delta_file(...).
+    This format is as returned from SpectraRay.read_psi_delta_file(...).
 
     Args:
         psi_delta (pandas.DataFrame): DataFrame containing Psi+Delta Measurement data
@@ -50,9 +51,10 @@ def calc_rho(psi_delta: pd.DataFrame) -> pd.DataFrame:
     Returns:
         pandas.DataFrame: Frame containing rho as an imaginary number.
     """
-    return psi_delta.apply(lambda x: np.tan(np.deg2rad(x['Ψ'])) *
-                           np.exp(-1j * np.deg2rad(x['Δ'])),
-                           axis=1)
+    return psi_delta.apply(
+        lambda x: np.tan(np.deg2rad(x["Ψ"])) * np.exp(-1j * np.deg2rad(x["Δ"])), axis=1
+    )
+
 
 def get_qwp_thickness(material: "Material", lbda: float) -> float:
     """Return the thickness of a material in nm for a quater wave plate at wavelength 'lbda'.
@@ -65,4 +67,4 @@ def get_qwp_thickness(material: "Material", lbda: float) -> float:
         float: Thickness (in nm) of quarter wave plate
     """
     nr = np.real(material.get_refractive_index(lbda)[0, 0, 0])
-    return lbda / (4.*nr)
+    return lbda / (4.0 * nr)
