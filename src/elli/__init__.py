e92e583ee0ba7c4a951ff9cd956af34ba6d6274c
domna <florian.dobener@physik.hu-berlin.de>
2023-01-21 13:52:46 +0100

    Changes package method to pyproject.toml (fixes #103)

--- src/elli/__init__.py
+++ src/elli/__init__.py
@@ -14,17 +14,3 @@ from .solver2x2 import Solver2x2
 from .solver4x4 import *
 from .structure import *
 from .utils import *
-
-if sys.version_info.major >= 3 and sys.version_info.minor > 7:
-    from importlib.metadata import PackageNotFoundError, version
-else:
-    from importlib_metadata import PackageNotFoundError, version
-
-try:
-    # Change here if project is renamed and does not equal the package name
-    dist_name = __name__
-    __version__ = version(dist_name)
-except PackageNotFoundError:  # pragma: no cover
-    __version__ = "unknown"
-finally:
-    del version, PackageNotFoundError
