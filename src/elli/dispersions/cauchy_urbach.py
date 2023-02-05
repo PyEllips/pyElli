c711552df322b893f4394f18831634fb30cb33d0
MarJMue <49639740+MarJMue@users.noreply.github.com>
2023-02-05 23:38:59 +0100

    Update to new IndexDispersion

--- src/elli/dispersions/cauchy_urbach.py
+++ src/elli/dispersions/cauchy_urbach.py
@@ -1,12 +1,13 @@
 # Encoding: utf-8
-"""Cauchy dispersion."""
+"""Cauchy dispersion, with Urbach tail."""
+import numpy as np
 import numpy.typing as npt
 
-from .base_dispersion import Dispersion
+from .base_dispersion import IndexDispersion
 from ..utils import conversion_wavelength_energy
 
 
-class CauchyUrbach(Dispersion):
+class CauchyUrbach(IndexDispersion):
     r"""Cauchy dispersion, with an Urbach Tail absorption.
 
     Single parameters:
@@ -21,14 +22,18 @@ class CauchyUrbach(Dispersion):
         --
 
     Output:
+        .. math::
+            n(E) =
+            \boldsymbol{n_0} + \boldsymbol{B} E^2 + \boldsymbol{C} E^4
+            + i \boldsymbol{D} \exp (\frac{E - \boldsymbol{E_g}}{\boldsymbol{E_u}})
     """
 
-    single_params_template = {"n0": 1.5, "B": 0, "C": 0, "D": 0, "Eg": 0, "Eu": 1}
+    single_params_template = {"n0": 1.5, "B": 0, "C": 0, "D": 0, "Eg": 2, "Eu": 0.5}
     rep_params_template = {}
 
-    def dielectric_function(self, lbda: npt.ArrayLike) -> npt.NDArray:
+    def refractive_index(self, lbda: npt.ArrayLike) -> npt.NDArray:
         energy = conversion_wavelength_energy(lbda)
-        refr_index = (
+        return (
             self.single_params.get("n0")
             + self.single_params.get("B") * energy**2
             + self.single_params.get("C") * energy**4
@@ -38,4 +43,3 @@ class CauchyUrbach(Dispersion):
                 (energy - self.single_params.get("Eg")) / self.single_params.get("Eu")
             )
         )
-        return refr_index**2
