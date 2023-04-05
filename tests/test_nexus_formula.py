62a72c7fecd08ceeb2d16fa72135e93a823f6a9e
domna <florian.dobener@physik.hu-berlin.de>
2023-04-05 17:01:28 +0200

    Re-enable index based nexus file

--- tests/test_nexus_formula.py
+++ tests/test_nexus_formula.py
@@ -7,8 +7,4 @@ from elli.importer.nexus import read_nexus_materials
 def test_loading_without_errors(datadir):
     """All dispersive materials are loaded without errors"""
     for file in datadir.iterdir():
-        # Skip index based adding
-        # TODO: Implement index based adding and re-enable this test-file
-        if str(file).rsplit("/", 1)[-1] in ["Si-Chandler-Horowitz.nxs"]:
-            continue
         read_nexus_materials(file)
