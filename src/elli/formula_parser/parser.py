4cb8b827e9735861c19a80afaaee27b4880fc575
Florian Dobener <github@schroedingerscat.org>
2023-04-23 21:56:32 +0200

    Adds caching for formula dispersion (#138)
    * Adds caching for formula dispersion
    
    * Uses lru_cache to keep backwards compatibility
    
    * Adds maxsize in lru_cache for py3.7

--- src/elli/formula_parser/parser.py
+++ src/elli/formula_parser/parser.py
@@ -1,27 +1,10 @@
-#
-# Copyright The NOMAD Authors.
-#
-# This file is part of NOMAD. See https://nomad-lab.eu for further info.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 """This modules creates a formula parser"""
+from functools import lru_cache
 import os
 from operator import add, mul, neg, sub, truediv
 from typing import Dict
 
 import numpy as np
-import numpy.typing as npt
 import scipy.constants as sc
 from lark import Lark, Transformer, v_args
 from scipy.special import dawsn  # pylint: disable=no-name-in-module
@@ -173,67 +156,28 @@ class FormulaTransformer(Transformer):
         raise ValueError(f"No such parameter {name}")
 
 
-def formula_parser(transformer=None):
-    """Creates a formula parser.
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-    Args:
-        transformer (lark.Transformer, optional):
-            A transformer to use with this parsing.
-            If set to None it generates the AST.
-            Defaults to None.
-
-    Returns:
-        lark.Lark: The parsing object.
-    """
-    __location__ = os.path.realpath(
-        os.path.join(os.getcwd(), os.path.dirname(__file__))
+with open(
+    f"{__location__}/dispersion_function_grammar.lark", encoding="utf-8"
+) as formula_grammar:
+    grammar = Lark(
+        formula_grammar,
+        start="assignment",
+        parser="lalr",
     )
 
-    with open(
-        f"{__location__}/dispersion_function_grammar.lark", encoding="utf-8"
-    ) as formula_grammar:
-        if transformer is None:
-            return Lark(
-                formula_grammar,
-                start="assignment",
-                parser="lalr",
-            )
 
-        return Lark(
-            formula_grammar, start="assignment", parser="lalr", transformer=transformer
-        )
-
-
-def transformation_formula_parser(
-    x_axis_name: str,
-    x_axis_values: npt.ArrayLike,
-    single_params: Dict[str, float],
-    repeated_params: Dict[str, np.ndarray],
-):
-    """Creates a formula parser with the given parameters.
-    This parser is automatically evaluated based on the given parameters.
-    This is done directly whitout creating the abstract syntax tree.
+@lru_cache(maxsize=128)
+def parse_formula(formula: str):
+    """
+    Parses a dispersion formula string into an abstract syntax tree.
+    Uses caching to avoid re-parsing of the formula.
 
     Args:
-        x_axis_name (str): The name of the x-axis to use replace in the formula string.
-        x_axis_values (np.ndarray): The value array of the x-axis to replace in the formula string.
-        single_params (Dict[str, float]):
-            A dictionary of parameter name and value of single parameters.
-        repeated_params (Dict[str, np.ndarray]):
-            A dictionary of parameter name and values of repeated paremters.
+        formula (str): The formula string to parse.
 
     Returns:
-        str, np.ndarray:
-            The evaluated formula.
-            The first tuple element indicates the type of dispersion
-            and may either be `n` for refractive index or `eps` for dielectric function.
-            The second element is the evluated formula one point for each x-axis input point.
+        Lark.Tree: The parsed formula Tree.
     """
-    return formula_parser(
-        FormulaTransformer(
-            x_axis_name=x_axis_name,
-            x_axis_values=x_axis_values,
-            single_params=single_params,
-            repeated_params=repeated_params,
-        )
-    )
+    return grammar.parse(formula)
