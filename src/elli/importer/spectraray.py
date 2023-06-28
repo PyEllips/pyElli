8cd103159bea1e08646be5ffdf16046238fb7e4c
MarJMue <49639740+MarJMue@users.noreply.github.com>
2023-06-28 16:25:05 +0200

    change splitting to allow regex expressions

--- src/elli/importer/spectraray.py
+++ src/elli/importer/spectraray.py
@@ -2,13 +2,15 @@
 It only supplies a rudimentary loading of standard psi/delta values
 and misses some other features.
 """
+import re
+
 import pandas as pd
 
 from ..utils import calc_rho
 
 
 def read_spectraray_psi_delta(
-    fname: str, sep: str = " ", decimal: str = "."
+    fname: str, sep: str = r"\s+", decimal: str = "."
 ) -> pd.DataFrame:
     r"""Read a psi/delta spectraray ascii file.
     Only reads the first entry and does not support reading multiple angles.
@@ -41,7 +43,7 @@ def read_spectraray_psi_delta(
     with open(fname) as f:
         header = f.readlines()[0]
 
-    aois = list(map(float, header.split(sep)[2::2]))
+    aois = list(map(float, re.split(sep, header)[3::2]))
     index = pd.MultiIndex.from_product(
         [aois, ["Ψ", "Δ"]], names=["Angle of Incidence", ""]
     )
@@ -62,7 +64,7 @@ def read_spectraray_psi_delta(
 
 
 def read_spectraray_mmatrix(
-    fname: str, sep: str = r" ", decimal: str = "."
+    fname: str, sep: str = r"\s+", decimal: str = "."
 ) -> pd.DataFrame:
     r"""Read a mueller matrix spectraray ascii file.
     Only reads the first entry and does not support reading multiple angles.
@@ -104,7 +106,7 @@ def read_spectraray_mmatrix(
 
 
 def read_spectraray_rho(
-    fname: str, sep: str = r" ", decimal: str = "."
+    fname: str, sep: str = r"\s+", decimal: str = "."
 ) -> pd.DataFrame:
     r"""Read a psi/delta spectraray ascii file and converts it to rho values.
     Only reads the first entry and does not support reading multiple angles.
