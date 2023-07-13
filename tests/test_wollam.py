d9d48c6d0edf4c0683571cdd0bab824570a49eb7
Florian Dobener <github@schroedingerscat.org>
2023-07-13 09:58:27 +0200

    Adds woollam importer (#148)
    * Adds woollam importer
    
    * Removes walrus operator
    
    * Removes error columns from dataframe
    
    * Corrects shape test w/o errs
    
    * Show proper error for tan/cos wvase data

---
+++ tests/test_wollam.py
@@ -0,0 +1,32 @@
+"""Tests for reading woollam"""
+import pytest
+from fixtures import datadir  # pylint: disable=unused-import
+import elli
+
+
+# pylint: disable=redefined-outer-name
+def test_reading_of_psi_delta_woollam(datadir):
+    """Psi/delta Spectraray file is read w/o errors"""
+    data_wvase = elli.read_woollam_psi_delta(datadir / "wvase_example.dat")
+    data_cease = elli.read_woollam_psi_delta(datadir / "complete_ease_example.dat")
+
+    assert data_wvase.shape == (542, 2)
+    assert data_cease.shape == (3263, 2)
+
+
+# pylint: disable=redefined-outer-name
+def test_reading_and_conv_to_woollam(datadir):
+    """Rho values are read from Psi / Delta file"""
+    data_wvase = elli.read_woollam_rho(datadir / "wvase_example.dat")
+    data_cease = elli.read_woollam_rho(datadir / "complete_ease_example.dat")
+
+    assert data_wvase.shape == (542,)
+    assert data_cease.shape == (3263,)
+
+
+# pylint: disable=redefined-outer-name
+def test_raises_not_implemented_for_tan_cos_format(datadir):
+    """Raises error when a wvase Tan(Psi)/Cos(Delta) file is presented"""
+
+    with pytest.raises(NotImplementedError):
+        elli.read_woollam_psi_delta(datadir / "wvase_trig.dat")
