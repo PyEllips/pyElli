1dc18ab28145712d30e2ca80d34c9d0365d4af9c
Marius Müller <49639740+MarJMue@users.noreply.github.com>
2024-07-19 11:07:01 +0200

    Correctly raise an error in _convert_index

--- src/elli/result.py
+++ src/elli/result.py
@@ -46,7 +46,7 @@ def _convert_index(index: str) -> int:
     if index in ["s", "R"]:
         return 1
 
-    return ValueError("Wrong index given for variable.")
+    raise ValueError("Wrong index given for variable.")
 
 
 class Result:
